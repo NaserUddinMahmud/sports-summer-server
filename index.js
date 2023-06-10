e224a218680acefe99b0dcb04f193410d7503d6a
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-06-11 05:28:16 +0600

    users post function for social login added

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function run() {
     // users related APIs
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already created" });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
