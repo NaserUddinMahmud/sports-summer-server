2c922a2aa7d24eaa5f89ee9ba1a89231f42abeb9
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-06-11 21:08:58 +0600

    JWT verification added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,21 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+
+const verifyJWT = (req, res, next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6lu5yx.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -39,11 +55,17 @@ async function run() {
       .collection("selectedClasses");
     const usersCollection = client.db("sportsSummerDB").collection("users");
 
-    // users related APIs
-    app.get('/users', async(req, res) => {
-        const result = await usersCollection.find().toArray();
-        res.send(result);
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '8h' })
+        res.send({token})
     })
+
+    // users related APIs
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -56,17 +78,42 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/users/instructor/:id', async(req, res) =>{
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const updateDoc = {
-            $set: {
-              role: 'instructor'
-            },
-          };
-          const result = await usersCollection.updateOne(filter,updateDoc);
-          res.send(result)
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email',verifyJWT,  async(req,res) =>{
+        const email = req.params.email
+
+        if(req.decoded.email !== email){
+            res.send({admin:false})
+        }
+
+        const query = {email: email}
+        const user = await usersCollection.findOne(query);
+        const result = {admin: user?.role === 'admin'}
+        res.send(result);
     })
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     //   classes related APIs
     app.get("/classes", async (req, res) => {
@@ -81,10 +128,14 @@ async function run() {
     });
 
     // selected classes related APIs
-    app.get("/selectedClasses", async (req, res) => {
+    app.get("/selectedClasses", verifyJWT,  async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbidden access'})
       }
       const query = { email: email };
       const result = await selectedClassesCollection.find(query).toArray();
