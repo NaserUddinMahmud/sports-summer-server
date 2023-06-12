8dab9332f15dcf6b319e74c5dbed9dc24fe0fbac
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-06-12 12:17:45 +0600

    instructor verification added

--- index.js
+++ index.js
@@ -70,6 +70,15 @@ async function run() {
         }
         next();
     }
+    const verifyInstructor = async(req, res, next) =>{
+        const email = req.decoded.email;
+        const query = {email: email}
+        const user = await usersCollection.findOne(query);
+        if(user?.role !== 'instructor'){
+            return res.status(403).send({error:true, message: 'forbidden access'})
+        }
+        next();
+    }
 
     // users related APIs
     app.get("/users",verifyJWT, verifyAdmin, async (req, res) => {
@@ -99,6 +108,19 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+    app.get('/users/instructor/:email',verifyJWT,  async(req,res) =>{
+        const email = req.params.email
+
+        if(req.decoded.email !== email){
+            res.send({instructor:false})
+        }
+
+        const query = {email: email}
+        const user = await usersCollection.findOne(query);
+        const result = {instructor: user?.role === 'instructor'}
+        res.send(result);
+    })
 
     app.get('/users/admin/:email',verifyJWT,  async(req,res) =>{
         const email = req.params.email
